a42855d79d60ccb99bcd6ee8bf15e291dfd3e550
numega <drone_2008@mail.ru>
2017-07-31 16:35:29 +0300

    reconfigure templates settings

--- project/settings.py
+++ project/settings.py
@@ -134,6 +134,4 @@ LOGGING = {
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
