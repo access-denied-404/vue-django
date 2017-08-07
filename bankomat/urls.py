170d37fa13c6e5bec398a2abcbc21c455c271885
numega <drone_2008@mail.ru>
2017-08-07 20:12:48 +0300

    fill views, templates and urls according to documentation structure

--- bankomat/urls.py
+++ bankomat/urls.py
@@ -3,4 +3,17 @@ from bankomat import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+
+    url(r'^login$', views.LoginView.as_view(), name='login'),
+    url(r'^logout$', views.LogoutView.as_view(), name='logout'),
+    url(r'^register$', views.RegisterView.as_view(), name='register'),
+    url(r'^password_reset/request$', views.PasswordResetRequestView.as_view(), name='password_reset_request'),
+    url(r'^password_reset/reset$', views.PasswordResetResetView.as_view(), name='password_reset_reset'),
+
+    url(r'^cabinet$', views.CabinetView.as_view(), name='cabinet'),
+    url(r'^cabinet/requests$', views.CabinetRequestsView.as_view(), name='cabinet_requests'),
+    url(r'^cabinet/requests/new$', views.CabinetRequestsNewView.as_view(), name='cabinet_requests_new'),
+    url(r'^cabinet/requests/(?P<rid>\d+)$', views.CabinetRequestView.as_view(), name='cabinet_request'),
+    url(r'^cabinet/requests/(?P<rid>\d+)/banks$', views.CabinetRequestBanksView.as_view(), name='cabinet_request_banks'),
+    url(r'^cabinet/settings$', views.CabinetSettingsView.as_view(), name='cabinet_settings'),
 ]
--- bankomat/views.py
+++ bankomat/views.py
@@ -1,5 +1,49 @@
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 
 
 class IndexView(TemplateView):
     template_name = 'bankomat/index.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'bankomat/login.html'
+
+
+class RegisterView(TemplateView):
+    template_name = 'bankomat/register.html'
+
+
+class PasswordResetRequestView(TemplateView):
+    template_name = 'bankomat/password_reset_request.html'
+
+
+class PasswordResetResetView(TemplateView):
+    template_name = 'bankomat/password_reset_reset.html'
+
+
+class CabinetView(TemplateView):
+    template_name = 'bankomat/cabinet.html'
+
+
+class CabinetRequestsView(TemplateView):
+    template_name = 'bankomat/cabinet_requests.html'
+
+
+class CabinetRequestsNewView(TemplateView):
+    template_name = 'bankomat/cabinet_requests_new.html'
+
+
+class CabinetRequestView(TemplateView):
+    template_name = 'bankomat/cabinet_request.html'
+
+
+class CabinetRequestBanksView(TemplateView):
+    template_name = 'bankomat/cabinet_request_banks.html'
+
+
+class CabinetSettingsView(TemplateView):
+    template_name = 'bankomat/cabinet_settings.html'
+
+
+class LogoutView(RedirectView):
+    pattern_name = 'index'
