5e5741ba1ffe8af4efe9dbbfd244a1ca56bee07e
numega <drone_2008@mail.ru>
2017-08-18 18:50:08 +0300

    implemented finance products managing in admin panel

--- marer/admin.py
+++ marer/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
+from marer import models
+from mptt.admin import MPTTModelAdmin
 
-# Register your models here.
+
+@register(models.FinanceProduct)
+class FinanceProductAdmin(MPTTModelAdmin):
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
