d45049319ccdd1d3c8504ce7cb65e8e37c75268a
numega <drone_2008@mail.ru>
2017-08-22 17:42:22 +0300

    added form loading in public pages

--- marer/views/__init__.py
+++ marer/views/__init__.py
@@ -13,7 +13,17 @@ class IndexView(TemplateView):
     template_name = 'marer/index.html'
 
     def get(self, request, *args, **kwargs):
-        quick_request_form = QuickRequestForm()
+        if request.user.id:
+            quick_request_form = QuickRequestForm(initial=dict(
+                contact_person_name=request.user.get_full_name(),
+                contact_email=request.user.email,
+                contact_phone=request.user.phone,
+            ))
+            quick_request_form.fields['contact_person_name'].disabled = True
+            quick_request_form.fields['contact_email'].disabled = True
+            quick_request_form.fields['contact_phone'].disabled = True
+        else:
+            quick_request_form = QuickRequestForm()
         finance_product_roots = FinanceProduct.objects.root_nodes()
         context_part = dict(
             finance_product_roots=finance_product_roots,
@@ -22,13 +32,29 @@ class IndexView(TemplateView):
         kwargs.update(context_part)
         return super().get(request, *args, **kwargs)
 
+    def post(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
+
 
 class FinanceProductView(TemplateView):
     template_name = 'marer/product.html'
 
     def get(self, request, *args, **kwargs):
-        quick_request_form = QuickRequestForm()
         product = get_object_or_404(FinanceProduct, id=kwargs.get('pid', 0))
+        if request.user.id:
+            quick_request_form = QuickRequestForm(initial=dict(
+                finance_product=product.id,
+                contact_person_name=request.user.get_full_name(),
+                contact_email=request.user.email,
+                contact_phone=request.user.phone,
+            ))
+            quick_request_form.fields['contact_person_name'].disabled = True
+            quick_request_form.fields['contact_email'].disabled = True
+            quick_request_form.fields['contact_phone'].disabled = True
+        else:
+            quick_request_form = QuickRequestForm(initial=dict(
+                finance_product=product.id,
+            ))
         if product.childrens.exists():
             raise Http404()
         finance_product_roots = FinanceProduct.objects.root_nodes()
@@ -39,6 +65,9 @@ class FinanceProductView(TemplateView):
         )
         kwargs.update(context_part)
         return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
 
 
 class CabinetRequestsView(LoginRequiredMixin, TemplateView):
