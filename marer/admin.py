1fea06b0bb2e14c80d81e4bfd494d6f60f11f368
numega <drone_2008@mail.ru>
2017-08-23 14:48:39 +0300

    regrouped finance products admin form fields

--- marer/admin.py
+++ marer/admin.py
@@ -1,10 +1,21 @@
 from django.contrib.admin import ModelAdmin, register
+from django.utils.translation import ugettext_lazy as _
 from marer import models
 from mptt.admin import MPTTModelAdmin
 
 
 @register(models.FinanceProduct)
 class FinanceProductAdmin(MPTTModelAdmin):
+    fieldsets = (
+        (None, dict(fields=(
+            ('name', 'parent'),
+            'page_content',
+        ))),
+        (_('SEO'), dict(classes='collapse', fields=(
+            ('_seo_h1', '_seo_title'),
+            ('_seo_description', '_seo_keywords'),
+        ))),
+    )
 
     def has_add_permission(self, request):
         return False
