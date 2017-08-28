67fc41bfe560fdc3e686da564e994410da969775
numega <drone_2008@mail.ru>
2017-08-28 13:17:00 +0300

    updated issue templates and views with issue context

--- marer/views/issue.py
+++ marer/views/issue.py
@@ -1,38 +1,51 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 
+from marer.models import Issue
 
-class IssueRegisteringView(LoginRequiredMixin, TemplateView):
+
+class IssueView(LoginRequiredMixin, TemplateView):
+    template_name = ''
+
+    def get(self, request, *args, **kwargs):
+        iid = kwargs.get('iid', 0)
+        issue = get_object_or_404(Issue, id=iid)
+        kwargs.update(issue=issue)
+        return super().get(request, *args, **kwargs)
+
+
+class IssueRegisteringView(IssueView):
     template_name = 'marer/issue/registering.html'
 
 
-class IssueCommonDocumentsRequestView(LoginRequiredMixin, TemplateView):
+class IssueCommonDocumentsRequestView(IssueView):
     template_name = 'marer/issue/common_documents_request.html'
 
 
-class IssueSurveyView(LoginRequiredMixin, TemplateView):
+class IssueSurveyView(IssueView):
     template_name = 'marer/issue/survey.html'
 
 
-class IssueScoringView(LoginRequiredMixin, TemplateView):
+class IssueScoringView(IssueView):
     template_name = 'marer/issue/scoring.html'
 
 
-class IssueAdditionalDocumentsRequestView(LoginRequiredMixin, TemplateView):
+class IssueAdditionalDocumentsRequestView(IssueView):
     template_name = 'marer/issue/additional_documents_request.html'
 
 
-class IssuePaymentsView(LoginRequiredMixin, TemplateView):
+class IssuePaymentsView(IssueView):
     template_name = 'marer/issue/payments.html'
 
 
-class IssueFinalDocumentsApprovalView(LoginRequiredMixin, TemplateView):
+class IssueFinalDocumentsApprovalView(IssueView):
     template_name = 'marer/issue/final_documents_approval.html'
 
 
-class IssueFinishedView(LoginRequiredMixin, TemplateView):
+class IssueFinishedView(IssueView):
     template_name = 'marer/issue/finished.html'
 
 
-class IssueCancelledView(LoginRequiredMixin, TemplateView):
+class IssueCancelledView(IssueView):
     template_name = 'marer/issue/cancelled.html'
