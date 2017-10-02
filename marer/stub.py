2a70d6e99ef2c7110259ec03480d0ccf2ebb963a
numega <drone_2008@mail.ru>
2017-10-02 18:12:54 +0300

    moved getting test tender data to stub module

--- marer/stub.py
+++ marer/stub.py
@@ -2,7 +2,11 @@
 All functions and classes described here are stubs.
 All of it must be re-declared and re-implemented somewhere else.
 """
+from django.utils import timezone
+
+from marer import consts
 from marer.models import Issuer
+from marer.serializers import TenderPublisherSerializer, TenderSerializer
 
 
 def create_stub_issuer(user_owner, issuer_name):
@@ -17,3 +21,25 @@ def create_stub_issuer(user_owner, issuer_name):
     issuer.save()
     return issuer
 
+
+def get_serialized_tender():
+    publisher = TenderPublisherSerializer(dict(
+        full_name='ООО Тестовая организация',
+        legal_address='Не дом и не улица',
+        inn='0000000000',
+        ogrn='000000000000',
+        kpp='00000000',
+    ))
+
+    tender = TenderSerializer(dict(
+        gos_number='0000000000000000',
+        law=consts.TENDER_EXEC_LAW_44_FZ,
+        placement_type='Открытый аукцион',
+        publish_datetime=timezone.localtime(timezone.now()),
+        start_cost=100500.00,
+        application_ensure_cost=100.50,
+        contract_execution_ensure_cost=10050.00,
+        currency_code=consts.CURRENCY_RUR,
+        publisher=publisher.data,
+    ))
+    return tender
--- marer/views/rest.py
+++ marer/views/rest.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 
 from marer.forms import RestTenderForm
 from marer.serializers import TenderSerializer, TenderPublisherSerializer
+from marer.stub import get_serialized_tender
 
 
 class TenderDataView(APIView):
@@ -11,27 +12,9 @@ class TenderDataView(APIView):
         # TODO make errors on errors if occurs
         rtform = RestTenderForm(request.GET)
         if not rtform.is_valid():
-            tdata = TenderSerializer()
+            tender = TenderSerializer()
 
         else:
-            publisher = TenderPublisherSerializer(dict(
-                full_name = 'ООО Тестовая организация',
-                legal_address='Не дом и не улица',
-                inn='0000000000',
-                ogrn='000000000000',
-                kpp='00000000',
-            ))
+            tender = get_serialized_tender()
 
-            tdata = TenderSerializer(dict(
-                gos_number='0000000000000000',
-                law=TenderSerializer.LAW_44_FZ,
-                placement_type='Открытый аукцион',
-                publish_datetime=timezone.localtime(timezone.now()),
-                start_cost=100500.00,
-                application_ensure_cost=100.50,
-                contract_execution_ensure_cost=10050.00,
-                currency_code=TenderSerializer.CURRENCY_RUR,
-                publisher=publisher.data,
-            ))
-
-        return Response(tdata.data)
+        return Response(tender.data)
