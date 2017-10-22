8485cf168b353cd45f9bedd420ae102a77fe14e4
numega <drone_2008@mail.ru>
2017-10-22 23:08:49 +0300

    moved all utility functions from loading finance orgs conditions to separate module of utils package

--- marer/management/commands/loadfoc.py
+++ marer/management/commands/loadfoc.py
@@ -1,5 +1,4 @@
 import logging
-import re
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
@@ -7,6 +6,8 @@ from openpyxl import load_workbook
 
 from marer.models.finance_org import FinanceOrgProductConditions, FinanceOrganization
 from marer.products import BankGuaranteeProduct
+from marer.utils.loadfoc import get_cell_percentage, get_cell_bool, get_cell_review_term_days, \
+    get_cell_ensure_condition, get_cell_summ_range, get_cell_value
 
 logger = logging.getLogger('django')
 
@@ -74,7 +75,7 @@ class Command(BaseCommand):
         """
         idx = 4
 
-        bank_name = self._get_cell_data(sh, 'a', idx).value
+        bank_name = get_cell_value(sh, 'a', idx).value
         while bank_name is not None and bank_name != '':
 
             new_foc = FinanceOrgProductConditions()
@@ -82,60 +83,60 @@ class Command(BaseCommand):
             try:
                 # Subtypes conditions
                 # 44-FZ
-                min_sum, max_sum = self._get_cell_summ_range(self._get_cell_data(sh, 'b', idx))
+                min_sum, max_sum = get_cell_summ_range(get_cell_value(sh, 'b', idx))
                 new_foc.bg_44_app_ensure_min_sum = min_sum
                 new_foc.bg_44_app_ensure_max_sum = max_sum
-                new_foc.bg_44_app_ensure_interest_rate = self._get_cell_percentage(self._get_cell_data(sh, 'c', idx))
+                new_foc.bg_44_app_ensure_interest_rate = get_cell_percentage(get_cell_value(sh, 'c', idx))
 
-                min_sum, max_sum = self._get_cell_summ_range(self._get_cell_data(sh, 'd', idx))
+                min_sum, max_sum = get_cell_summ_range(get_cell_value(sh, 'd', idx))
                 new_foc.bg_44_contract_exec_min_sum = min_sum
                 new_foc.bg_44_contract_exec_max_sum = max_sum
-                new_foc.bg_44_contract_exec_interest_rate = self._get_cell_percentage(self._get_cell_data(sh, 'e', idx))
+                new_foc.bg_44_contract_exec_interest_rate = get_cell_percentage(get_cell_value(sh, 'e', idx))
 
                 # 223-FZ
-                min_sum, max_sum = self._get_cell_summ_range(self._get_cell_data(sh, 'f', idx))
+                min_sum, max_sum = get_cell_summ_range(get_cell_value(sh, 'f', idx))
                 new_foc.bg_223_app_ensure_min_sum = min_sum
                 new_foc.bg_223_app_ensure_max_sum = max_sum
-                new_foc.bg_223_app_ensure_interest_rate = self._get_cell_percentage(self._get_cell_data(sh, 'g', idx))
+                new_foc.bg_223_app_ensure_interest_rate = get_cell_percentage(get_cell_value(sh, 'g', idx))
 
-                min_sum, max_sum = self._get_cell_summ_range(self._get_cell_data(sh, 'h', idx))
+                min_sum, max_sum = get_cell_summ_range(get_cell_value(sh, 'h', idx))
                 new_foc.bg_223_contract_exec_min_sum = min_sum
                 new_foc.bg_223_contract_exec_max_sum = max_sum
-                new_foc.bg_223_contract_exec_interest_rate = self._get_cell_percentage(self._get_cell_data(sh, 'i', idx))
+                new_foc.bg_223_contract_exec_interest_rate = get_cell_percentage(get_cell_value(sh, 'i', idx))
 
 
                 # 185-FZ
-                min_sum, max_sum = self._get_cell_summ_range(self._get_cell_data(sh, 'j', idx))
+                min_sum, max_sum = get_cell_summ_range(get_cell_value(sh, 'j', idx))
                 new_foc.bg_185_min_sum = min_sum
                 new_foc.bg_185_max_sum = max_sum
-                new_foc.bg_185_interest_rate = self._get_cell_percentage(self._get_cell_data(sh, 'k', idx))
+                new_foc.bg_185_interest_rate = get_cell_percentage(get_cell_value(sh, 'k', idx))
 
                 # Commercial
-                min_sum, max_sum = self._get_cell_summ_range(self._get_cell_data(sh, 'l', idx))
+                min_sum, max_sum = get_cell_summ_range(get_cell_value(sh, 'l', idx))
                 new_foc.bg_ct_min_sum = min_sum
                 new_foc.bg_ct_max_sum = max_sum
-                new_foc.bg_ct_interest_rate = self._get_cell_percentage(self._get_cell_data(sh, 'm', idx))
+                new_foc.bg_ct_interest_rate = get_cell_percentage(get_cell_value(sh, 'm', idx))
 
                 # VAT
-                min_sum, max_sum = self._get_cell_summ_range(self._get_cell_data(sh, 'n', idx))
+                min_sum, max_sum = get_cell_summ_range(get_cell_value(sh, 'n', idx))
                 new_foc.bg_vat_min_sum = min_sum
                 new_foc.bg_vat_max_sum = max_sum
-                new_foc.bg_vat_interest_rate = self._get_cell_percentage(self._get_cell_data(sh, 'o', idx))
+                new_foc.bg_vat_interest_rate = get_cell_percentage(get_cell_value(sh, 'o', idx))
 
                 # Customs
-                min_sum, max_sum = self._get_cell_summ_range(self._get_cell_data(sh, 'p', idx))
+                min_sum, max_sum = get_cell_summ_range(get_cell_value(sh, 'p', idx))
                 new_foc.bg_customs_min_sum = min_sum
                 new_foc.bg_customs_max_sum = max_sum
-                new_foc.bg_customs_interest_rate = self._get_cell_percentage(self._get_cell_data(sh, 'q', idx))
+                new_foc.bg_customs_interest_rate = get_cell_percentage(get_cell_value(sh, 'q', idx))
 
                 # Base conditions
-                new_foc.personal_presence_required = self._get_cell_bool(self._get_cell_data(sh, 'r', idx))
-                new_foc.bg_review_term_days = self._get_cell_review_term_days(self._get_cell_data(sh, 's', idx))
+                new_foc.personal_presence_required = get_cell_bool(get_cell_value(sh, 'r', idx))
+                new_foc.bg_review_term_days = get_cell_review_term_days(get_cell_value(sh, 's', idx))
 
-                ensure_type, ensure_value = self._get_cell_ensure_condition(self._get_cell_data(sh, 't', idx))
+                ensure_type, ensure_value = get_cell_ensure_condition(get_cell_value(sh, 't', idx))
                 new_foc.bg_insurance_type = ensure_type
                 new_foc.bg_insurance_value = ensure_value
-                new_foc.bg_bank_account_opening_required = self._get_cell_bool(self._get_cell_data(sh, 'u', idx))
+                new_foc.bg_bank_account_opening_required = get_cell_bool(get_cell_value(sh, 'u', idx))
             except Exception:
                 logger.warning("Error on parsing line {}, finance organization {}".format(idx, bank_name))
 
@@ -149,176 +150,4 @@ class Command(BaseCommand):
             new_foc.finance_product = product.name
             new_foc.save()
             idx += 1
-            bank_name = self._get_cell_data(sh, 'a', idx).value
-
-    def _get_cell_summ_range(self, cell_data):
-
-        if cell_data.value is None or cell_data.value == '':
-            return None, None
-
-        # variants:
-        #   ' от ХХХ млн '
-        #   ' до ХХХ млн '
-        #   ' от ХХХ млн до ХХХ млн'
-        #   ' ХХХ млн - ХХХ млн '
-
-        min_sum = None
-        max_sum = None
-
-        patterns = [
-            re.compile('\s*от\s*(?P<min_sum>\d+)\s*(?P<min_mult>\w*)\s*до\s*(?P<max_sum>\d+)\s*(?P<max_mult>\w*)\s*'),
-            re.compile('\s*от\s*(?P<min_sum>\d+)\s*(?P<min_mult>\w*)\s*'),
-            re.compile('\s*до\s*(?P<max_sum>\d+)\s*(?P<max_mult>\w*)\s*'),
-            re.compile('\s*(?P<min_sum>\d+)\s*(?P<min_mult>\w*)\s*-\s*(?P<max_sum>\d+)\s*(?P<max_mult>\w*)\s*'),
-        ]
-        for pattern in patterns:
-            matches = pattern.fullmatch(str(cell_data.value).lower())
-            if matches:
-                multipliers = {
-                    'тыс': 1000,
-                    'млн': 1000000,
-                    'млрд': 1000000000,
-                }
-
-                match_data = matches.groupdict()
-
-                min_sum = match_data.get('min_sum', None)
-                if min_sum is not None:
-                    min_mult = multipliers.get(match_data.get('min_mult', None), 1)
-                    min_sum = int(min_sum) * min_mult
-
-                max_sum = match_data.get('max_sum', None)
-                if max_sum is not None:
-                    max_mult = multipliers.get(match_data.get('max_mult', None), 1)
-                    max_sum = int(max_sum) * max_mult
-
-                break
-
-        return min_sum, max_sum
-
-    def _get_cell_percentage(self, cell_data):
-
-        if cell_data.value is None or cell_data.value == '':
-            return None
-
-        # variants
-        #   ' 1,2 % '
-        #   ' 2 % '
-        #   ' 3.5 % '
-
-        percentage = None
-
-        patterns = [
-            re.compile('\s*(?P<int_part>\d+)[.,](?P<decimal_part>\d+)\s*%?\s*'),
-            re.compile('\s*(?P<int_part>\d+)\s*%?\s*'),
-        ]
-        for pattern in patterns:
-            matches = pattern.fullmatch(cell_data.value)
-            if matches:
-                match_data = matches.groupdict()
-                percentage = float('{int_part}.{decimal_part}'.format(
-                    int_part=match_data.get('int_part', 0),
-                    decimal_part=match_data.get('decimal_part', 0),
-                ))
-                break
-
-        return percentage
-
-    def _get_cell_bool(self, cell_data):
-
-        if cell_data.value is None or cell_data.value == '':
-            return False
-
-        # variants
-        #   ' да '
-        #   ' нет '
-        #   ' + '
-        #   ' - '
-
-        patterns_true = [
-            re.compile('\s*да\s*'),
-            re.compile('\s*\+\s*'),
-        ]
-        patterns_false = [
-            re.compile('\s*нет\s*'),
-            re.compile('\s*-\s*'),
-        ]
-
-        for pattern in patterns_true:
-            matches = pattern.fullmatch(str(cell_data.value).lower())
-            if matches:
-                return True
-
-        for pattern in patterns_false:
-            matches = pattern.fullmatch(str(cell_data.value).lower())
-            if matches:
-                return False
-
-        return False
-
-    def _get_cell_review_term_days(self, cell_data):
-
-        if cell_data.value is None or cell_data.value == '':
-            return 0
-
-        # variants
-        #   ' 10 '
-        #   ' 10 дней '
-        #   ' 2 дня '
-        #   ' 10 д '
-        #   ' 10 дн '
-        #   ' 10 дн. '
-
-        pattern = re.compile('\s*(?P<days>\d+)\s*(дней|дня|д|дн|дн.)?\s*')
-
-        matches = pattern.fullmatch(str(cell_data.value).lower())
-        if matches:
-            days_data = matches.groupdict().get('days', None)
-            return int(days_data) if days_data else 0
-
-        return 0
-
-    def _get_cell_ensure_condition(self, cell_data):
-
-        if cell_data.value is None or cell_data.value == '':
-            return None, 0
-
-        # variants
-        #   ' залог 10 % '
-        #   ' 10 % '
-        #   ' недвижимость '
-        #   ' нет '
-
-        patterns_pledge = [
-            re.compile('\s*залог\s*(?P<percentage>\d+)\s*%\s*'),
-            re.compile('\s*(?P<percentage>\d+)\s*%\s*'),
-        ]
-        patterns_estate = [
-            re.compile('\s*недвижимость\s*'),
-            re.compile('\s*-\s*'),
-        ]
-        patterns_none = [
-            re.compile('\s*нет\s*'),
-        ]
-
-        for pattern in patterns_pledge:
-            matches = pattern.fullmatch(str(cell_data.value).lower())
-            if matches:
-                ens_data = matches.groupdict()
-                ensure_type = FinanceOrgProductConditions.INSURANCE_TYPE_PLEDGE
-                ensure_value = int(ens_data.get('percentage'))
-                return ensure_type, ensure_value
-
-        for pattern in patterns_estate:
-            matches = pattern.fullmatch(str(cell_data.value).lower())
-            if matches:
-                return FinanceOrgProductConditions.INSURANCE_TYPE_REAL_ESTATE, 100
-
-        for pattern in patterns_none:
-            matches = pattern.fullmatch(str(cell_data.value).lower())
-            if matches:
-                return None, 0
-
-    def _get_cell_data(self, sheet, col, row):
-        sheet_idx = '{col}{row}'.format(col=str(col).upper(), row=row)
-        return sheet[sheet_idx]
+            bank_name = get_cell_value(sh, 'a', idx).value
---
+++ marer/utils/loadfoc.py
@@ -0,0 +1,181 @@
+import re
+
+from marer.models.finance_org import FinanceOrgProductConditions
+
+
+def get_cell_percentage(cell_data):
+
+    if cell_data.value is None or cell_data.value == '':
+        return None
+
+    # variants
+    #   ' 1,2 % '
+    #   ' 2 % '
+    #   ' 3.5 % '
+
+    percentage = None
+
+    patterns = [
+        re.compile('\s*(?P<int_part>\d+)[.,](?P<decimal_part>\d+)\s*%?\s*'),
+        re.compile('\s*(?P<int_part>\d+)\s*%?\s*'),
+    ]
+    for pattern in patterns:
+        matches = pattern.fullmatch(cell_data.value)
+        if matches:
+            match_data = matches.groupdict()
+            percentage = float('{int_part}.{decimal_part}'.format(
+                int_part=match_data.get('int_part', 0),
+                decimal_part=match_data.get('decimal_part', 0),
+            ))
+            break
+
+    return percentage
+
+
+def get_cell_bool(cell_data):
+
+    if cell_data.value is None or cell_data.value == '':
+        return False
+
+    # variants
+    #   ' да '
+    #   ' нет '
+    #   ' + '
+    #   ' - '
+
+    patterns_true = [
+        re.compile('\s*да\s*'),
+        re.compile('\s*\+\s*'),
+    ]
+    patterns_false = [
+        re.compile('\s*нет\s*'),
+        re.compile('\s*-\s*'),
+    ]
+
+    for pattern in patterns_true:
+        matches = pattern.fullmatch(str(cell_data.value).lower())
+        if matches:
+            return True
+
+    for pattern in patterns_false:
+        matches = pattern.fullmatch(str(cell_data.value).lower())
+        if matches:
+            return False
+
+    return False
+
+
+def get_cell_review_term_days(cell_data):
+
+    if cell_data.value is None or cell_data.value == '':
+        return 0
+
+    # variants
+    #   ' 10 '
+    #   ' 10 дней '
+    #   ' 2 дня '
+    #   ' 10 д '
+    #   ' 10 дн '
+    #   ' 10 дн. '
+
+    pattern = re.compile('\s*(?P<days>\d+)\s*(дней|дня|д|дн|дн.)?\s*')
+
+    matches = pattern.fullmatch(str(cell_data.value).lower())
+    if matches:
+        days_data = matches.groupdict().get('days', None)
+        return int(days_data) if days_data else 0
+
+    return 0
+
+
+def get_cell_ensure_condition(cell_data):
+
+    if cell_data.value is None or cell_data.value == '':
+        return None, 0
+
+    # variants
+    #   ' залог 10 % '
+    #   ' 10 % '
+    #   ' недвижимость '
+    #   ' нет '
+
+    patterns_pledge = [
+        re.compile('\s*залог\s*(?P<percentage>\d+)\s*%\s*'),
+        re.compile('\s*(?P<percentage>\d+)\s*%\s*'),
+    ]
+    patterns_estate = [
+        re.compile('\s*недвижимость\s*'),
+        re.compile('\s*-\s*'),
+    ]
+    patterns_none = [
+        re.compile('\s*нет\s*'),
+    ]
+
+    for pattern in patterns_pledge:
+        matches = pattern.fullmatch(str(cell_data.value).lower())
+        if matches:
+            ens_data = matches.groupdict()
+            ensure_type = FinanceOrgProductConditions.INSURANCE_TYPE_PLEDGE
+            ensure_value = int(ens_data.get('percentage'))
+            return ensure_type, ensure_value
+
+    for pattern in patterns_estate:
+        matches = pattern.fullmatch(str(cell_data.value).lower())
+        if matches:
+            return FinanceOrgProductConditions.INSURANCE_TYPE_REAL_ESTATE, 100
+
+    for pattern in patterns_none:
+        matches = pattern.fullmatch(str(cell_data.value).lower())
+        if matches:
+            return None, 0
+
+
+def get_cell_summ_range(cell_data):
+
+    if cell_data.value is None or cell_data.value == '':
+        return None, None
+
+    # variants:
+    #   ' от ХХХ млн '
+    #   ' до ХХХ млн '
+    #   ' от ХХХ млн до ХХХ млн'
+    #   ' ХХХ млн - ХХХ млн '
+
+    min_sum = None
+    max_sum = None
+
+    patterns = [
+        re.compile('\s*от\s*(?P<min_sum>\d+)\s*(?P<min_mult>\w*)\s*до\s*(?P<max_sum>\d+)\s*(?P<max_mult>\w*)\s*'),
+        re.compile('\s*от\s*(?P<min_sum>\d+)\s*(?P<min_mult>\w*)\s*'),
+        re.compile('\s*до\s*(?P<max_sum>\d+)\s*(?P<max_mult>\w*)\s*'),
+        re.compile('\s*(?P<min_sum>\d+)\s*(?P<min_mult>\w*)\s*-\s*(?P<max_sum>\d+)\s*(?P<max_mult>\w*)\s*'),
+    ]
+    for pattern in patterns:
+        matches = pattern.fullmatch(str(cell_data.value).lower())
+        if matches:
+            multipliers = {
+                'тыс': 1000,
+                'млн': 1000000,
+                'млрд': 1000000000,
+            }
+
+            match_data = matches.groupdict()
+
+            min_sum = match_data.get('min_sum', None)
+            if min_sum is not None:
+                min_mult = multipliers.get(match_data.get('min_mult', None), 1)
+                min_sum = int(min_sum) * min_mult
+
+            max_sum = match_data.get('max_sum', None)
+            if max_sum is not None:
+                max_mult = multipliers.get(match_data.get('max_mult', None), 1)
+                max_sum = int(max_sum) * max_mult
+
+            break
+
+    return min_sum, max_sum
+
+
+def get_cell_value(sheet, col, row):
+    sheet_idx = '{col}{row}'.format(col=str(col).upper(), row=row)
+    return sheet[sheet_idx]
