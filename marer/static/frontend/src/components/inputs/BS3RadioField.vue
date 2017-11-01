36e9bec86b4550a4ff58b87ac0af5daac498b6ab
numega <drone_2008@mail.ru>
2017-11-01 16:56:03 +0300

    implemented columns in Vue Bootstrap radio field options list

--- marer/static/frontend/src/components/inputs/BS3RadioField.vue
+++ marer/static/frontend/src/components/inputs/BS3RadioField.vue
@@ -3,14 +3,15 @@
     <label>
       <slot name="label">{{label}}</slot>
     </label>
-    <div v-for="option in options">
-      <radio
-        v-model="selected"
-        :name="name"
-        :selected-value="option.value"
-        type="primary"
-      >{{ option.text }}</radio>
+    <div class="row">
+      <div v-for="option in options" :class="colClass">
+        <radio v-model="selected" :name="name" :selected-value="option.value" type="primary">{{ option.text }}</radio>
+      </div>
     </div>
+
+
+
+
   </div>
 
 </template>
@@ -21,7 +22,7 @@
     components: {
       'radio': radio
     },
-    props: ['name', 'value', 'label', 'options'],
+    props: ['name', 'value', 'label', 'options', 'cols'],
     computed: {
       selected: {
         get () {
@@ -30,7 +31,26 @@
         set (value) {
           this.$emit('input', value)
         }
+      },
+      colClass: {
+        get () {
+          if (this.cols) {
+            // available values: 1, 2, 3, 4, 6, 12
+            let colSize = Math.floor(12 / this.cols)
+            if (colSize < 1) colSize = 1
+            if (colSize > 12) colSize = 12
+            return 'col-sm-' + colSize.toString()
+          } else {
+            return 'col-sm-12'
+          }
+        }
       }
     }
   }
 </script>
+
+<style scoped="default">
+  .radio {
+    margin: 2px 0;
+  }
+</style>
