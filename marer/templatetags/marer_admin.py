6ba6ab1ead5184ccb0f50cd4d6f43db32452d7e6
numega <drone_2008@mail.ru>
2017-11-20 13:21:57 +0300

    added settings value template tag

--- marer/templatetags/marer_admin.py
+++ marer/templatetags/marer_admin.py
@@ -1,10 +1,16 @@
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
 
 def alter_add_url(inline_adm, parent_obj):
     return inline_adm.get_alter_add_url(parent_obj)
+
+
+@register.simple_tag
+def settings_value(name):
+    return getattr(settings, name, '')
 
 
 register.filter('alter_add_url', alter_add_url)
