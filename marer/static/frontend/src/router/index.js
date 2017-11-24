f2cd81cc0f3100b76e4184c9bf444699c68637fb
numega <drone_2008@mail.ru>
2017-11-24 04:07:10 +0300

    remove unused products

--- marer/static/frontend/src/router/index.js
+++ marer/static/frontend/src/router/index.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Stub from '@/components/Stub'
 import FormBankGuarantee from '@/components/FormBankGuarantee'
-import FormCredit from '@/components/FormCredit'
-import FormFactoring from '@/components/FormFactoring'
-import FormLeasing from '@/components/FormLeasing'
 
 Vue.use(Router)
 
@@ -19,21 +16,6 @@ export default new Router({
       path: '/BankGuaranteeProduct',
       name: 'FormBankGuarantee',
       component: FormBankGuarantee
-    },
-    {
-      path: '/CreditProduct',
-      name: 'CreditProduct',
-      component: FormCredit
-    },
-    {
-      path: '/FactoringProduct',
-      name: 'FactoringProduct',
-      component: FormFactoring
-    },
-    {
-      path: '/LeasingProduct',
-      name: 'LeasingProduct',
-      component: FormLeasing
     }
   ]
 })
