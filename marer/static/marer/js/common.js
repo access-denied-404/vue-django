e7a56c7c46c09e4f6b6ad6458f0652c03fa98342
numega <drone_2008@mail.ru>
2018-02-01 03:44:43 +0300

    fixed survey form required fields check

--- marer/static/marer/js/common.js
+++ marer/static/marer/js/common.js
@@ -2,15 +2,13 @@ $(function () {
     $('body').on('click', '.spinjs', function (e) {
         var check = $('.check');
         if (check.length > 0) {
-            if (check.val() !== 'blank') {
+            if (check.val() !== '' && check.val() !== undefined) {
                 setTimeout(function () {
                     $('body').append($('<img>', {
                         src: '/static/marer/img/rolling.gif',
                         style: 'position: fixed; border: 1px solid #ccc;width: 80px; z-index: 999; top: 45%; left: 50%;background: rgba(255, 255, 255, 0.75); padding: 5px;'
                     }));
                 }, 1);
-            } else {
-                event.preventDefault();
             }
         } else {
             setTimeout(function () {
