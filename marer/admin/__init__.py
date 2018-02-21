d3377e8c401ca4a57fda879b6fe2763f3f54cd75
numega <drone_2008@mail.ru>
2018-02-21 15:11:20 +0300

    implement legal name for users in admin

--- marer/admin/__init__.py
+++ marer/admin/__init__.py
@@ -703,7 +703,7 @@ class MarerUserAdmin(UserAdmin):
     def get_fieldsets(self, request, obj=None):
         full_fieldsets = (
             (None, {'fields': ('username', 'password', 'manager',)}),
-            (_('Personal info'), {'fields': ('first_name', 'middle_name', 'last_name', 'email', 'phone', 'comment')}),
+            (_('Personal info'), {'fields': ('first_name', 'middle_name', 'last_name', 'legal_name', 'email', 'phone', 'comment')}),
             (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_broker',
                                            'groups', 'user_permissions')}),
             (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
@@ -711,7 +711,7 @@ class MarerUserAdmin(UserAdmin):
         )
         limited_fieldsets = (
             (None, {'fields': ('username', 'password',)}),
-            (_('Personal info'), {'fields': ('first_name', 'middle_name', 'last_name', 'email', 'phone', 'comment')}),
+            (_('Personal info'), {'fields': ('first_name', 'middle_name', 'last_name', 'legal_name', 'email', 'phone', 'comment')}),
             (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
         )
         if obj:
